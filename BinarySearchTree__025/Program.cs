d77b0f9156730d9b97a79cdc740ae5c36be091dd
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-12-30 17:15:29 +0700

    class node

--- BinarySearchTree__025/Program.cs
+++ BinarySearchTree__025/Program.cs
@@ -6,6 +6,22 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree__025
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+    /* A node class consist os three things, the information, reference to the 
+       right child, and reference to the left child */
     internal class Program
     {
         static void Main(string[] args)
