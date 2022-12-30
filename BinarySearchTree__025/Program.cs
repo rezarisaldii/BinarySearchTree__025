c41ddc63b13bba8e3100dde015b89e4f13de8216
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-12-30 17:52:34 +0700

    done

--- BinarySearchTree__025/Program.cs
+++ BinarySearchTree__025/Program.cs
@@ -22,10 +22,110 @@ namespace BinarySearchTree__025
     }
     /* A node class consist os three things, the information, reference to the 
        right child, and reference to the left child */
-    internal class Program
+
+    class BinaryTree
     {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; /* Initializing ROOT to null */
+        }
+        public void insert(string element) /*Insert a node in the binary search tree*/
+        {
+            Node tmp, parent = null, currentNode = null;
+            Search(element, ref parent, ref currentNode);
+            if (currentNode != null) /*Check if the node to be inserted already inserted or not*/
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else /* If the spesified node is not present */
+            {
+                tmp = new Node(element, null, null); /* crates a node*/
+                if (parent == null) /* If the trees is empty*/
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            /*This function searches the currentNode of the speified Node as 
+             well as the current node of its parent*/
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
+        public void inorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.Write(ptr.info + " ");
+                inorder(ptr.rightchild);
+            }
+        }
+        public void preorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.Write(ptr.info + " ");
+                preorder(ptr.leftchild);
+                preorder(ptr.rightchild);
+            }
+        }
+        public void postorder(Node ptr) /* Performs the postorder traversal of the tree */
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.Write(ptr.info + " ");
+            }
+        }
         static void Main(string[] args)
         {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+
+            }
         }
     }
 }
